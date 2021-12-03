8e8e57ca6a8bd87cc7f254275f4d760c07e5f415
Marco <marco.pampaloni1@gmail.com>
2021-12-03 19:03:05 +0100

    Add steering wheel PID controller
    - Use angular commands from the STP to steer the vehicle (there is
    currently a bug in STP v1.1.2 that causes the angle to suddenly change
    direction)
    - Add min/max threshold values in PIDController
    - Add initial estimate of PID coefficients for the steering controller
    - Add naive implementation of quaternion -> euler angles (stolen from
    the internet)

--- src/execution/scripts/execution/dispatcher.py
+++ src/execution/scripts/execution/dispatcher.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
+import math
 import rospy
 import numpy as np
 import carla_msgs.msg
 from execution import topics
 from execution.PIDController import PIDController
+from execution.geometry import quaternion_to_euler
 from planning.msg import STP_Data
 from std_msgs.msg import String, Float32, Bool
 from geometry_msgs.msg import Accel
-
 
 class Dispatcher():
     def __init__(self):
@@ -17,7 +18,14 @@ class Dispatcher():
 
         self.current_velocity = 0.0
         self.target_velocity = 0.0
-        self.velocity_control = PIDController(self.current_velocity, Kp=6, Kd=0)
+        self.current_zorient = 0.0
+        self.target_zorient = 0.0
+
+        # Controllers
+        self.velocity_control = PIDController(self.current_velocity,
+                Kp=6, Kd=0, minv=0, maxv=1)
+        self.steering_control = PIDController(self.current_zorient,
+                Kp=1.0, Ki=1.5, Kd=1.0, minv=-0.9, maxv=0.9)
 
         # Steering wheel
         self.steer_enable_pub = rospy.Publisher(topics.STEER_ENABLE, Bool)
@@ -50,12 +58,12 @@ class Dispatcher():
         # CARLA vehicle information
         self.status_sub = rospy.Subscriber('/carla/ego_vehicle/vehicle_status',
                 carla_msgs.msg.CarlaEgoVehicleStatus, lambda data: self.update_status(data))
-        self.info_sub = rospy.Subscriber('/carla/ego_vehicle/vehicle_info',
-                carla_msgs.msg.CarlaEgoVehicleInfo, lambda data: self.log_msg(data))
+        # self.info_sub = rospy.Subscriber('/carla/ego_vehicle/vehicle_info',
+        #         carla_msgs.msg.CarlaEgoVehicleInfo, lambda data: self.log_msg(data))
 
         # STP stub subscription
         self.stp_sub = rospy.Subscriber('stp_data', STP_Data,
-                lambda data: self.update_throttle(data))
+                lambda data: self.update_control(data))
 
         # Initialize actuators states
         self.init_actuators()
@@ -67,27 +75,29 @@ class Dispatcher():
         pass
 
     def update_status(self, data):
-        rospy.loginfo(data)
+        # rospy.loginfo(data)
         # get current linear (forward) velocity
         self.current_velocity = data.velocity
+        q = data.orientation
+        (x, y, z) = quaternion_to_euler(q.x, q.y, q.z, q.w)
+        self.current_zorient = z
 
-
-    def update_throttle(self, data):
+    def update_control(self, data):
         # Get target velocity
         self.target_velocity = self.current_velocity + data.dv
-
-        # get current linear (forward) velocity
-        current = self.current_velocity
+        self.target_zorient = data.psi - np.pi/2.0
+        rospy.loginfo('current z: {:.3f}\t target z: {:.3}\t psi: {:.3}'.format(self.current_zorient, self.target_zorient, data.psi))
 
         # Vehicle Control message
         cmd = carla_msgs.msg.CarlaEgoVehicleControl()
-        cmd.throttle = self.velocity_control.pid_step(current, self.target_velocity)
+
+        # Control throttle
+        cmd.throttle = self.velocity_control.pid_step(self.current_velocity, self.target_velocity)
+        # Control steering
+        cmd.steer = -self.steering_control.pid_step(self.current_zorient, self.target_zorient)
+
         self.cmd_pub.publish(cmd)
 
-    def get_target_acceleration(self):
-        # target acceleration stub
-        return 2.0 # m/s^2
-    
     def spin(self):
         rate = rospy.Rate(100) # 100hz
         while not rospy.is_shutdown():
