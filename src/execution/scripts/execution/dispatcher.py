c62f2cd0403c64d6c6e77ec41cbb524e40da840d
Marco <marco.pampaloni1@gmail.com>
2021-12-04 14:23:01 +0100

    Fix braking controller bug
    - Reverse proportionality of braking PID controller
    - Tune PID parameters

--- src/execution/scripts/execution/dispatcher.py
+++ src/execution/scripts/execution/dispatcher.py
@@ -20,14 +20,15 @@ class Dispatcher():
         self.target_velocity = 0.0
         self.current_zorient = 0.0
         self.target_zorient = 0.0
+        self.last_cmd = None
 
         # Controllers
         self.velocity_control = PIDController(self.current_velocity,
-                Kp=6, Kd=0, minv=0, maxv=1)
+                Kp=3.0, Ki=1.5, Kd=0.0, minv=0, maxv=1)
         self.steering_control = PIDController(self.current_zorient,
-                Kp=1.5, Ki=1.2, Kd=1.0, minv=-0.9, maxv=0.9)
+                Kp=0.6, Ki=0.3, Kd=0.0, minv=-0.9, maxv=0.9)
         self.braking_control = PIDController(self.current_zorient,
-                Kp=3.0, Ki=1.5, Kd=1.0, minv=0.0, maxv=1.0)
+                Kp=5.5, Ki=1.5, Kd=0.0, minv=0.0, maxv=1.0)
 
         # Steering wheel
         self.steer_enable_pub = rospy.Publisher(topics.STEER_ENABLE, Bool)
@@ -65,7 +66,7 @@ class Dispatcher():
 
         # STP stub subscription
         self.stp_sub = rospy.Subscriber('stp_data', STP_Data,
-                lambda data: self.update_control(data))
+                lambda data: self.update_command(data))
 
         # Initialize actuators states
         self.init_actuators()
@@ -75,6 +76,9 @@ class Dispatcher():
 
     def init_actuators(self):
         pass
+    
+    def update_command(self, data):
+        self.last_cmd = data
 
     def update_status(self, data):
         # rospy.loginfo(data)
@@ -89,8 +93,8 @@ class Dispatcher():
         self.target_velocity = self.current_velocity + data.dv
         # self.target_zorient = data.psi - np.pi/2.0
         self.target_zorient = 0.0
-        rospy.loginfo('current z: {:.3f}\t target z: {:.3}\t psi: {:.3}'.format(self.current_zorient, self.target_zorient, data.psi))
-
+        rospy.loginfo('current z: {:.3f}\t target z: {:.3}'.format(self.current_zorient, self.target_zorient))
+        rospy.loginfo('current v: {:.3f}\t target v: {:.3}'.format(self.current_velocity, self.target_velocity))
         # Vehicle Control message
         cmd = carla_msgs.msg.CarlaEgoVehicleControl()
 
@@ -99,15 +103,25 @@ class Dispatcher():
         # Control steering
         cmd.steer = -self.steering_control.pid_step(self.current_zorient, self.target_zorient)
         # Control braking (if not activating the throttle)
+        brake = 1 - self.braking_control.pid_step(self.current_velocity, self.target_velocity)
         if data.dv < 0 and cmd.throttle <= 1e-2:
-            cmd.brake = self.steering_control.pid_step(self.current_velocity, self.target_velocity)
+            cmd.brake = brake
 
+        # Deactivate steering when braking
+        # if cmd.brake > 0.0 or self.target_velocity < 1.0:
+        #     cmd.steer = 0.0
 
+        rospy.loginfo('current brake: {:.3f}'.format(cmd.brake))
+        rospy.loginfo('current steer: {:.3f}'.format(cmd.steer))
+        rospy.loginfo('current throttle: {:.3f}'.format(cmd.throttle))
+        rospy.loginfo(10*'-')
         self.cmd_pub.publish(cmd)
 
     def spin(self):
-        rate = rospy.Rate(100) # 100hz
+        rate = rospy.Rate(15) # 100hz
         while not rospy.is_shutdown():
+            if self.last_cmd:
+                self.update_control(self.last_cmd)
             rate.sleep()
 
 
